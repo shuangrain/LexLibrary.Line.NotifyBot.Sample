b7bac91f61c04ac7d5eb5736a2b24356150a6c55
lex.xu <shuangrain07@gmail.com>
2018-12-23 13:43:44 +0800

    加入專案檔案。

---
+++ LexLibrary.Line.Login.Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LexLibrary.Line.Login.Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
